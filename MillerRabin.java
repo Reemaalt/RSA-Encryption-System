491e843e622da83827aa9d6bda4df7c878b994ad
al3alema-Batata <137813106+al3alema-Batata@users.noreply.github.com>
2023-11-22 10:31:50 +0300

    added miller rabin primality test

---
+++ MillerRabin.java
@@ -0,0 +1,64 @@
+import java.security.SecureRandom;
+public class MillerRabin {
+
+    private static final SecureRandom random = new SecureRandom();
+
+    public static boolean singleTest(long n, int a) {
+        long exp = n - 1;//assuming n is prime n-1 is even
+
+        while (exp % 2 == 0) //trying to make exp an odd number
+        {
+            exp >>= 1;
+        }
+
+        if (power(a, exp, n) == 1) //modular exponintion is a^exp ≡1(modn)
+        {
+            return true;//it's composite
+        }
+
+        while (exp < n - 1) {
+            if (power(a, exp, n) == n - 1) {
+                return true;
+            }
+            exp <<= 1;
+        }
+
+        return false;
+    }
+
+    public static boolean millerRabinTest(long n, int k) {
+        for (int i = 0; i < k; i++) {
+            int a = getRandomInt(2, (int) (n - 1));
+            if (!singleTest(n, a)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(millerRabinTest(71, 40));
+    }
+
+    private static int getRandomInt(int min, int max)
+    {
+        return random.nextInt(max - min + 1) + min;
+    }
+
+    private static long power(long base, long exp, long mod)
+    {
+        long result = 1;
+        base = base % mod;
+
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                result = (result * base) % mod;
+            }
+            exp >>= 1;
+            base = (base * base) % mod;
+        }
+
+        return result;
+    }
+}
--- test.java
+++
@@ -1,5 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println("I'm in Aicha's Branch");
-    }
-}
